9c6e56ecaaa46315ea9f1962cf2653d1129c9112
AlexisSalazar123 <alexissalazar123g@gmail.com>
2023-10-18 22:06:52 -0500

    Cambiar direccion a index

--- Login/index.php
+++ Login/index.php
@@ -17,7 +17,7 @@
                 $contraseña_ingresada = $_POST['contraseña'];
 
                 if ($contraseña_bd == $contraseña_ingresada) {
-                    header("Location: inicio.php"); 
+                    header("Location: index.php"); 
                 } else {
                     $mensajeError = "La contraseña o el usuario son incorrectos.";
                 }
