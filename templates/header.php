2c9148c37a4e87ed2accb5f4827498254e08e4fd
AlexisSalazar123 <alexissalazar123g@gmail.com>
2023-10-22 19:25:13 -0500

    Update header.php in img

--- templates/header.php
+++ templates/header.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="Css/estilosHeader.css">
+    <link rel="stylesheet" href="../Css/estilosHeader.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 </head>
@@ -12,56 +12,56 @@
 
 <div class="navbar" id="navVertical">
 
-      <img src="img/LogoBerriondo.png" alt="Logo" style="width:225px;" class="rounded">
+      <img src="../img/LogoBerriondo.png" alt="Logo" style="width:225px;" class="rounded">
 
       <div class="horizontal-line"></div>
    
       <ul class="category-list">
   <li>
     <div class="category-item">
-      <img src="img/icons/home.svg" id="iconHome">
+      <img src="../img/icons/home.svg" id="iconHome">
       <a href="#">Inicio</a>
     </div>
   </li>
   <li>
     <div class="category-item">
-      <img src="img/icons/box.svg" id="iconHome">
+      <img src="../img/icons/box.svg" id="iconHome">
       <a href="#">Inventario</a>
     </div>
   </li>
   <li>
     <div class="category-item">
-      <img src="img/icons/production.svg" id="iconHome">
+      <img src="../img/icons/production.svg" id="iconHome">
       <a href="#">Producción</a>
     </div>
   </li>
   <li>
     <div class="category-item">
-      <img src="img/icons/sales.svg" id="iconHome">
+      <img src="../img/icons/sales.svg" id="iconHome">
       <a href="#">Ventas</a>
     </div>
   </li>
   <li>
     <div class="category-item">
-      <img src="img/icons/arepa.png" id="iconHome">
+      <img src="../img/icons/arepa.png" id="iconHome">
       <a href="#">Productos</a>
     </div>
   </li>
   <li>
     <div class="category-item">
-      <img src="img/icons/customers.svg" id="iconHome">
+      <img src="../img/icons/customers.svg" id="iconHome">
       <a href="#">Clientes</a>
     </div>
   </li>
   <li>
     <div class="category-item">
-      <img src="img/icons/provider.svg" id="iconHome">
+      <img src="../img/icons/provider.svg" id="iconHome">
       <a href="#">Proveedor</a>
     </div>
   </li>
   <li>
     <div class="category-item">
-      <img src="img/icons/user.svg" id="iconHome">
+      <img src="../img/icons/user.svg" id="iconHome">
       <a href="#">Usuarios</a>
     </div>
   </li>
@@ -80,14 +80,14 @@
 
   <div class="container-fluid" id="notification">
     <a class="navbar-brand" href="#">
-      <img src="img/icons/notification.svg" alt="Logo" style="width:25px;" class="rounded-pill">
+      <img src="../img/icons/notification.svg" alt="Logo" style="width:25px;" class="rounded-pill">
     </a>
   </div>
   
   <div class="vertical-line"></div>
   <div class="container-fluid" id="imgUsuario">
     <a class="navbar-brand" href="#">
-      <img src="img/Berriondo.jpeg" alt="Logo" style="width:55px;" class="rounded-pill">
+      <img src="../img/Berriondo.jpeg" alt="Logo" style="width:55px;" class="rounded-pill">
     </a>
     <p>Don Berriondo</p>
   </div>
