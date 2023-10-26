4ed9b1f9da455f64203a95bc0a06a80293f7fcf2
AlexisSalazar123 <alexissalazar123g@gmail.com>
2023-10-26 09:40:41 -0500

    Create update.php

---
+++ Produccion/update.php
@@ -0,0 +1,71 @@
+<?php
+include('../conexion.php');
+
+if(isset($_POST['numero'])){
+    $id = $_POST['numero'];
+    $codigo = $_POST['codigo'];
+    $producto = $_POST['producto'];
+    $cantidad = $_POST['cantidad'];
+    $fecha = $_POST['fecha'];
+
+    $sel = $con->query("SELECT cantidad FROM produccion WHERE id_produccion=". $id);
+    $cantidad_anterior = $sel->fetch_assoc();
+
+    $cantidad_anterior_valor = $cantidad_anterior['cantidad'];
+
+    $cantidad_arepas = $cantidad_anterior_valor * 5;
+    $cantidad_harina = ($cantidad_arepas / 5) * 0.5;
+    $cantidad_mozarella = ($cantidad_arepas / 5) * 0.25;
+    $cantidad_fresco = ($cantidad_arepas / 5) * 0.150;
+    $cantidad_azucar = ($cantidad_arepas / 5) * 0.02;
+    $cantidad_sal = ($cantidad_arepas / 5) * 0.01;
+    $cantidad_mantequilla = ($cantidad_arepas / 5) * 0.03;
+    $cantidad_agua = ($cantidad_arepas / 5) * 0.35;
+    $cantidad_leche = ($cantidad_arepas / 5) * 0.35;
+
+    $ins_harina = $con->query("UPDATE inventario SET cantidad = cantidad + $cantidad_harina WHERE nombre_ingrediente = 'Harina'");
+    $ins_mozarrella = $con->query("UPDATE inventario SET cantidad = cantidad + $cantidad_mozarella WHERE nombre_ingrediente = 'queso mozarella'");
+    $ins_fresco = $con->query("UPDATE inventario SET cantidad = cantidad + $cantidad_fresco WHERE nombre_ingrediente = 'queso fresco'");
+    $ins_azucar = $con->query("UPDATE inventario SET cantidad = cantidad + $cantidad_azucar WHERE nombre_ingrediente = 'azucar'");
+    $ins_sal = $con->query("UPDATE inventario SET cantidad = cantidad + $cantidad_sal WHERE nombre_ingrediente = 'sal'");
+    $ins_mantequilla = $con->query("UPDATE inventario SET cantidad = cantidad + $cantidad_mantequilla WHERE nombre_ingrediente = 'mantequilla'");
+    $ins_agua = $con->query("UPDATE inventario SET cantidad = cantidad + $cantidad_agua WHERE nombre_ingrediente = 'agua'");
+    $ins_leche = $con->query("UPDATE inventario SET cantidad = cantidad + $cantidad_leche WHERE nombre_ingrediente = 'leche'");
+
+
+    $cantidad_add = $cantidad * 5;
+    $cantidad_harina = ($cantidad_add / 5) * 0.5;
+    $cantidad_mozarella = ($cantidad_add / 5) * 0.25;
+    $cantidad_fresco = ($cantidad_add / 5) * 0.150;
+    $cantidad_azucar = ($cantidad_add / 5) * 0.02;
+    $cantidad_sal = ($cantidad_add / 5) * 0.01;
+    $cantidad_mantequilla = ($cantidad_add / 5) * 0.03;
+    $cantidad_agua = ($cantidad_add / 5) * 0.35;
+    $cantidad_leche = ($cantidad_add / 5) * 0.35;
+
+    $ins_harina = $con->query("UPDATE inventario SET cantidad = cantidad - $cantidad_harina WHERE nombre_ingrediente = 'Harina'");
+    $ins_mozarrella = $con->query("UPDATE inventario SET cantidad = cantidad - $cantidad_mozarella WHERE nombre_ingrediente = 'queso mozarella'");
+    $ins_fresco = $con->query("UPDATE inventario SET cantidad = cantidad - $cantidad_fresco WHERE nombre_ingrediente = 'queso fresco'");
+    $ins_azucar = $con->query("UPDATE inventario SET cantidad = cantidad - $cantidad_azucar WHERE nombre_ingrediente = 'azucar'");
+    $ins_sal = $con->query("UPDATE inventario SET cantidad = cantidad - $cantidad_sal WHERE nombre_ingrediente = 'sal'");
+    $ins_mantequilla = $con->query("UPDATE inventario SET cantidad = cantidad - $cantidad_mantequilla WHERE nombre_ingrediente = 'mantequilla'");
+    $ins_agua = $con->query("UPDATE inventario SET cantidad = cantidad - $cantidad_agua WHERE nombre_ingrediente = 'agua'");
+    $ins_leche = $con->query("UPDATE inventario SET cantidad = cantidad - $cantidad_leche WHERE nombre_ingrediente = 'leche'");
+
+    $up = $con->query("UPDATE produccion SET
+                     id_produccion='$id', codigo_produccion='$codigo', nombre_producto='$producto', cantidad='$cantidad', fecha='$fecha'
+                     WHERE id_produccion='$id' ");
+                
+
+if($up){
+
+
+    $mensaje="Registro actualizado";
+    header("location:index.php?mensaje=".$mensaje);
+}else{
+    header('location:index.php');
+}
+}
+
+
+?>
